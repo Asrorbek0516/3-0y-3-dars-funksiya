717bb3766388627565dd36a3d611edb8d47c9558
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-07-01 08:40:24 +0500

    funksiya 3-misol

---
+++ f3.py
@@ -0,0 +1,17 @@
+def ortacha_top(royxat):
+    if len(royxat)==0:
+        return 0
+    
+    return sum(royxat) / len(royxat)
+
+sonlar = [10, 20, 30, 40]
+
+print(ortacha_top(sonlar))
+
+def ortacha_top(royxat):
+    if len(royxat)==0:
+        return 0
+    
+    return sum(royxat) / len(royxat)
+
+print(ortacha_top([10,20,30,40]))
